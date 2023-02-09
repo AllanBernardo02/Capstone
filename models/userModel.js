61cf0b5730d4a6b7153c96beed5636d12808c134
Allan02 <85282679+AllanBernardo02@users.noreply.github.com>
2023-02-09 11:23:51 +0800

    Solve error #2

--- models/userModel.js
+++ models/userModel.js
@@ -1,50 +1,49 @@
-const mongoose = require ("mongoose");
+const mongoose = require("mongoose");
 const { required } = require("nodemon/lib/config");
 
 const userSchema = new mongoose.Schema(
-    {
-        name: {
-            type: "string",
-            required: true,
-        },
-        email: {
-            type: "string",
-            required: true,
-        },
-        password: {
-            type: "string",
-            required: true,
-        },
-
-        isDoctor: {
-            type: Boolean,
-            default: false,
-        },
-        isAdmin: {
-            type: Boolean,
-            default: false,
-        },
-        isApplyDoctor: {
-            type: Boolean,
-            default: false,
-        },
-        isBlocked: {
-            type: Boolean,
-            deafault: false,
-        },
-        seenNotification: {
-            type: Array,
-            default: [],
-        },
-        unseenNotification: {
-            type: Array,
-            default: []
-        },
-
+  {
+    name: {
+      type: "string",
+      required: true,
     },
-    {
-        timestamps: true,
-    }
+    email: {
+      type: "string",
+      required: true,
+    },
+    password: {
+      type: "string",
+      required: true,
+    },
+
+    isDoctor: {
+      type: Boolean,
+      default: false,
+    },
+    isAdmin: {
+      type: Boolean,
+      default: false,
+    },
+    isApplyDoctor: {
+      type: Boolean,
+      default: false,
+    },
+    isBlocked: {
+      type: Boolean,
+      deafault: false,
+    },
+    seenNotification: {
+      type: Array,
+      default: [],
+    },
+    unseenNotification: {
+      type: Array,
+      default: [],
+    },
+  },
+  {
+    timestamps: true,
+  }
 );
 
 const userModel = mongoose.model("Users", userSchema);
